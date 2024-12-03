22daf593aa7ab4710fe6942b355d73ec3396c5e7
haqqis20 <muhammadhaqqisyahputra@student.uir.ac.id>
2024-12-03 22:38:59 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello Bro! Syahrul Ramadhan</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d3d3d3', 
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20, 
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  box1: {
+    backgroundColor: 'yellow',
+    width: 100,
+    height: 100,
+  },
+  box2: {
+    backgroundColor: 'black',
+    width: 100,
+    height: 100,
   },
 });
